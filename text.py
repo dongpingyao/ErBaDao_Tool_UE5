372e34d6840e35eb5e909c3b3eea3c428ce915ac
dongpingyao <32844726+dongpingyao@users.noreply.github.com>
2022-08-23 23:45:47 +0800

    添加 脚本

--- text.py
+++ text.py
@@ -1,30 +1,14 @@
 # -*- coding: UTF-8 -*-#
-import zipfile
-
-import pywget as wget
-import shutil
+import wget
 import os
 
-url = 'http://182.92.66.60:45015/down/aV9E2zTCj2oS?fname=/ErBaDao_Tool_Maya.zip'
+
+url = 'https://codeload.github.com/dongpingyao/ErBaDao_Tool_UE5/zip/refs/heads/main'
 scriptPath = r"C:\Users\Alan\Downloads\ls\haha"
 filePath = r"C:\Users\Alan\Downloads\ls"
 fileName = filePath + r'\ErBaDao_Tool_Maya.zip'
-if os.path.exists(fileName):
-    os.remove(fileName)
-    print ('正在下载中请稍等')
-    wget.download(url, filePath)
-    print('下载完成')
-    print(fileName)
-else:
-    print ('正在下载中请稍等')
-    wget.download(url, filePath)
-    print('下载完成')
-    print(fileName)
+wget.download(url , filePath , (1024,1920,20))
 
-if os.path.exists(scriptPath + r"\ErBaDao_Tool_Maya"):
-    shutil.rmtree(scriptPath + r"\ErBaDao_Tool_Maya")
-    zip = zipfile.ZipFile(fileName)
-    zip.extractall(scriptPath)
-    zip.close()
-    print ("插件更新完成")
+
+
 
